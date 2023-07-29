9d9810772b2f226ed03b406120937399dac3820c
Adam Johnson <adam7819@googlemail.com>
2023-07-29 10:21:54 +0100

    Add array problem sets

---
+++ Arrays Problem Sets/Array Problem Sets.js
@@ -0,0 +1,23 @@
+// 1. Write a JavaScript function to check whether an `input` is an array or not.
+// Test Data :
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
+// false
+// true
+
+const is_array = (arr) => Array.isArray(arr);
+
+
+/*
+  * Task description: Write a method that creates a new array with given values 
+  * Expected Result: (3, 'a') => ['a', 'a', 'a'] 
+  * Task Complexity: 1 of 5 
+  * @param {number} arraySize - size of array 
+  * @param {?} value - value to fill 
+  * @returns {Array} 
+*/
+
+const fill = (arraySize, value) => {
+    arr = new Array(arraySize).fill(value);
+    return arr; 
+}
